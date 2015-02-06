a6c5eb5924f8ed7add5efebb28ba43e43a4e9d48
Aaron Schachter <aaronschachter@gmail.com>
2015-02-06 17:14:09 -0500

    Adds rocker gif to InboxZeroView

--- RBReviewer/DSOInboxZeroView.m
+++ RBReviewer/DSOInboxZeroView.m
@@ -7,6 +7,8 @@
 //
 
 #import "DSOInboxZeroView.h"
+#import "DSOInboxViewController.h"
+#import <UIImageView-PlayGIF/YFGIFImageView.h>
 
 @implementation DSOInboxZeroView
 -(void)layoutSubviews {
@@ -23,9 +25,22 @@
                                     options:nil];
         
         [self addSubview:self.view];
-        
+        NSData *gifData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"2000guitar.gif" ofType:nil]];
+ 
+        YFGIFImageView *gifView = [[YFGIFImageView alloc] initWithFrame:CGRectMake(-16, 0,  [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height / 2)];
+        gifView.gifData = gifData;
+        [self addSubview:gifView];
+        [gifView startGIF];
     }
     return self;
 }
 
+- (IBAction)rockOnTapped:(id)sender {
+    // @todo: Buggy, this only works the first tap, otherwise complains with Warning: Attempt to present <UINavigationController: 0x7fff3bda1c80> on <UINavigationController: 0x7fff3be1dee0> whose view is not in the window hierarchy!
+
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    UIViewController *rootViewController = window.rootViewController;
+    DSOInboxViewController *inboxVC = [rootViewController.storyboard instantiateViewControllerWithIdentifier:@"inboxNavigationController"];
+    [rootViewController presentViewController:inboxVC animated:YES completion:nil];
+}
 @end
