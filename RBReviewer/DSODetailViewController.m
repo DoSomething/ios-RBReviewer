18ba1f73243b0968d8c54fcbca6de3078d472e54
Aaron Schachter <aaronschachter@gmail.com>
2015-02-20 09:39:37 -0500

    Deletes ReviewTableViewCell

--- RBReviewer/DSODetailViewController.m
+++ RBReviewer/DSODetailViewController.m
@@ -82,7 +82,7 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 6;
+    return 5;
 }
 
 
@@ -97,7 +97,7 @@
             return cell;
         }
 
-        case 1: {
+        case 2: {
             DSOImageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
             NSData* imageData = [[NSData alloc]initWithContentsOfURL:[NSURL URLWithString:self.reportbackFile[@"src"]]];
             UIImage* image = [[UIImage alloc] initWithData:imageData];
@@ -105,7 +105,7 @@
             cell.selectionStyle = UITableViewCellSelectionStyleNone;
             return cell;
         }
-        case 2: {
+        case 3: {
             DSOCaptionTableViewCell *cell =  [tableView dequeueReusableCellWithIdentifier:@"captionCell" forIndexPath:indexPath];
             
             if ([self.reportbackFile[@"caption"] isEqualToString:@""]) {
@@ -118,7 +118,7 @@
             cell.selectionStyle = UITableViewCellSelectionStyleNone;
             return cell;
         }
-        case 3: {
+        case 1: {
             DSOQuantityTableViewCell *cell =  [tableView dequeueReusableCellWithIdentifier:@"quantityCell" forIndexPath:indexPath];
             cell.quantityLabel.text = nil;
             if (self.reportbackFile[@"quantity"] != nil) {
@@ -133,21 +133,6 @@
             DSODynamicTextTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"whyParticipatedCell" forIndexPath:indexPath];
             cell.dynamicTextLabel.text = self.reportbackFile[@"why_participated"];
             cell.selectionStyle = UITableViewCellSelectionStyleNone;
-            return cell;
-        }
-        case 5: {
-            DSOReviewTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reviewCell" forIndexPath:indexPath];
-            cell.selectionStyle = UITableViewCellSelectionStyleNone;
-
-            cell.excludeButton.tag = 0;
-            [cell.excludeButton addTarget:self action:@selector(review:) forControlEvents:UIControlEventTouchUpInside];
-
-            cell.approveButton.tag = 10;
-            [cell.approveButton addTarget:self action:@selector(review:) forControlEvents:UIControlEventTouchUpInside];
-
-            cell.promoteButton.tag = 20;
-            [cell.promoteButton addTarget:self action:@selector(review:) forControlEvents:UIControlEventTouchUpInside];
-            
             return cell;
         }
     }
